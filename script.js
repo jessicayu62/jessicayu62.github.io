ce03588b79a4701879923038bac0de1d15fac357
jessicayu62 <41123985+jessicayu62@users.noreply.github.com>
2020-08-23 11:31:03 -0700

    animations
    on timeline and hover on nav bar

--- script.js
+++ script.js
@@ -1,44 +1,84 @@
-//Get the button:
-mybutton = document.getElementById("myBtn");
+// //Get the button:
+// mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-	scrollFunction()
-};
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {
+// 	scrollFunction()
+// };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
+
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 400) {
+        $('#myBtn').fadeIn();
+    } else {
+        $('#myBtn').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#myBtn").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
 
 
 
-// $(document).scroll(function () {
-//     //Show element after user scrolls 800px
-//     var y = $(this).scrollTop();
-//     if (y > 0) {
-//         $('.bottomMenu').fadeIn();
-//     } else {
-//         $('.bottomMenu').fadeOut();
-//     }
+//Timeline Animation
 
-//     // Show element after user scrolls past 
-//     // the top edge of its parent 
-//     $('.container').each(function () {
-//         // var t = $(this).parent().offset().top;
-//         if (y > 100) {
-//             $(this).fadeIn();
-//         } else {
-//             $(this).fadeOut();
-//         }
-//     });
-// });
+(function($) {
+
+  $.fn.visible = function(partial) {
+    
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height(),
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;
+    
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+  };
+    
+})(jQuery);
+
+var win = $(window);
+
+var allMods = $(".content");
+
+allMods.each(function(i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible"); 
+  } 
+});
+
+win.scroll(function(event) {
+  
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in"); 
+    } 
+  });
+  
+});
+
