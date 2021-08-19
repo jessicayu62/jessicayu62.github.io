2db7ec44d07708ff78cc52db9593d4695b5e8ad4
jessicayu62 <41123985+jessicayu62@users.noreply.github.com>
2021-08-19 11:23:39 -0700

    Project section sizing

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,7 +8,7 @@ $(() => {
       var objectBottom = $(this).offset().top + $(this).outerHeight();
       var objectTop = $(this).offset().top + $(this).outerHeight() - $(this).height();
       /* If the element is out of bounds of the window then fade it out */
-      if (objectBottom - 115 < windowTop || objectTop + 115 > windowBottom) { // object fades out
+      if (objectBottom - 80 < windowTop || objectTop + 80 > windowBottom) { // object fades out
         if ($(this).css('opacity') == 1) {$(this).fadeTo(400,0);}
       } else { //object comes into view
         if ($(this).css('opacity') == 0) {$(this).fadeTo(400,1);}
